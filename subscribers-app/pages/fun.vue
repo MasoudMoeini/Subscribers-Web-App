a63116dba7eece3fd9335ede0a216c4811f4b185
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-08-15 15:39:21 +0200

    update

---
+++ subscribers-app/pages/fun.vue
@@ -0,0 +1,21 @@
+
+<title>Subscribers Application</title>
+<style>
+	body {
+	  background-color: rgb(7, 172, 198);
+	  color: rgb(255, 255, 255);
+	}
+	h1{
+    	text-align: center;
+    }
+	h2{
+    	text-align: center;
+    }
+
+	form{
+		text-align: center;
+	}
+	</style>
+<template>
+  <h1>Hello From Next Page</h1>
+</template>
---
+++ subscribers-app/pages/index.vue
@@ -0,0 +1,20 @@
+<title>Subscribers Application</title>
+<style>
+	body {
+	  background-color: rgb(7, 172, 198);
+	  color: rgb(255, 255, 255);
+	}
+	h1{
+    	text-align: center;
+    }
+	h2{
+    	text-align: center;
+    }
+
+	form{
+		text-align: center;
+	}
+	</style>
+<template>
+  <h1>Hello From Frontend</h1>
+</template>
