414c25306269097fa16f64a73b72421cac4849b5
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-08-24 17:44:50 +0200

    update

--- subscribers-app/pages/index.vue
+++ subscribers-app/pages/index.vue
@@ -15,9 +15,10 @@
     div{
       text-align: center;
     }
-   
-    
-     
+     p{ display: inline-block;
+       margin-left: auto;
+        margin-right: auto;
+      text-align: center;}
     label,input,button {
     display: block;
     float: left;
@@ -82,14 +83,67 @@ th, td {
         <input type="text" v-model="form.subscribedToChannel"/><br>
         <label>Id</label>
         <input type="text" v-model="form.id"/><br>
-        <button v-on:click="submitForm()">Submit</button>
-        <button v-on:click="updateForm()">Update</button><br>
-         <button v-on:click="">Reset</button>
-        <button v-on:click="deleteRecord()">Delete</button><br>
-        </form>
+        </form> 
     </div>
-    
+<p>
+<button  v-on:click="searchRecord()">Search</button>
+<button v-on:click="submitForm()">Submit</button>
+<button v-on:click="updateForm()">Update</button>
+<button v-on:click="deleteRecord()">Delete</button>
+<button v-on:click="reloadPage()">Reset</button><br>
+</p>
+  <div>
+      <v-simple-table  class="mx-auto">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Subscription Channel
+          </th>
+           <th class="text-left">
+            Subscription Date
+          </th>
+           <th class="text-left">
+            Subscribtion Id
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, i) in searchItem" :key="i"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.subscribedToChannel }}</td>
+          <td>{{ item.subscribeDate }}</td>
+          <td>{{ item._id}}</td>
+        </tr>
+      </tbody>
+  </v-simple-table>
+    </div><br>
     <div>
+      <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </v-card>
+
+    </div>
+    <!-- <div>
       <v-simple-table  height="350px">
       <thead>
         <tr>
@@ -118,7 +172,9 @@ th, td {
         </tr>
       </tbody>
   </v-simple-table>
-    </div>
+    </div> -->
+      
+
    </main>
 </template>
 
@@ -128,6 +184,19 @@ export default {
     data(){
         return {
           items:[],
+          searchItem:[],
+          search: '',
+          headers:[
+          {
+            text: 'Name',
+            align: 'start',
+            filterable: true,
+            value: 'name',
+          },
+          { text: 'Subscription Channel', value: 'subscribedToChannel' },
+          { text: 'Subscription Date', value: 'subscribeDate' },
+          { text: 'Subscribtion Id', value: '_id' },
+          ],
             form: {
                 name: '',
                 subscribedToChannel: '',
@@ -145,19 +214,38 @@ export default {
                 .then(function( response ){
                      // Handle success
                 }.bind(this));
+                this.reloadPage();
         },
         updateForm(){
             this.$axios.$patch(`/subscribers/${this.form.id}`, this.form)
                 .then(response => { 
 	              console.log(response)
                 })
+                this.reloadPage();
         },
         deleteRecord(){
            this.$axios.$delete(`/subscribers/${this.form.id}`, this.form)
                 .then(response => { 
 	              console.log(response)
               })
+              this.reloadPage();
         },
-    }
+        async searchRecord(){
+        const result= await this.$axios.$get(`/subscribers/${this.form.id}`, this.form);
+            this.searchItem = [result];
+           
+            
+       },
+       async getAllRecord(){
+        const result= await this.$axios.$get(`/subscribers/${this.form.id}`, this.form);
+            this.items = result;
+            this.reloadPage();
+            
+       },
+      reloadPage(){
+         window.location.reload()
+             }
+    },  
+    
 }
 </script>
